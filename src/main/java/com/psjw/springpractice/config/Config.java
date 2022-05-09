5d4dc2d5d84bf932f6930332fb74eecafa35dfbe
psjw <dlrudtn108@gmail.com>
2022-05-09 21:44:08 +0900

    Config #1 - Spring Configuration
    기초설정

---
+++ src/main/java/com/psjw/springpractice/config/Config.java
@@ -0,0 +1,9 @@
+package com.psjw.springpractice.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan("com.psjw.springpractice")
+@Configuration
+public class Config {
+}
