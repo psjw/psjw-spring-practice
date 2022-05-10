d1cf5f7e7e1308297d8647b96d65001d5b1fcbf2
psjw <dlrudtn108@gmail.com>
2022-05-10 09:27:29 +0900

    Apply Spring Framework Default Setting

--- src/main/java/com/psjw/springpractice/config/Config.java
+++ src/main/java/com/psjw/springpractice/config/Config.java
@@ -2,7 +2,9 @@ package com.psjw.springpractice.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @ComponentScan("com.psjw.springpractice")
 @Configuration
 public class Config {
